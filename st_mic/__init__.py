34baf23b37c670ba248d5475049ab950206db085
yeyoon7 <ooyy1110@gmail.com>
2024-09-19 21:50:05 +0900

    알림 박스 글자 중앙 정렬, 녹음 전,중,후 마다 알림박스 나오게

--- st_mic/__init__.py
+++ st_mic/__init__.py
@@ -18,6 +18,19 @@ else:
     build_dir = os.path.join(parent_dir, "frontend/build")
     _component_func = components.declare_component("st_mic", path=build_dir)
 
+# 텍스트 중앙 정렬을 위한 CSS 추가
+def apply_centered_text_css():
+    st.markdown(
+        """
+        <style>
+        .stAlert p {
+            text-align: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
 # 오디오 녹음 컴포넌트를 위한 래퍼 함수
 def st_mic(key=None):
     """Create a new instance of the audio recorder component.
@@ -37,25 +50,38 @@ def st_mic(key=None):
     # 컴포넌트를 호출하고 데이터 받아오기
     component_value = _component_func(key=key)
     
+    apply_centered_text_css()
+    
     if component_value is None:
-        st.error("No data received from the component.")
+        st.error("마이크 버튼을 누르면 녹음이 시작됩니다.")
         return None
 
-    #st.write(f"component_value: {component_value}")  # 전달된 데이터 확인 (디버깅용)
+    
+    # 딕셔너리에서 byteArray 추출
     if isinstance(component_value, dict):
-        # 딕셔너리에서 byteArray 추출
         byte_array = component_value.get("byteArray")
+        is_recording = component_value.get("isRecording", False)
+        
+        # 상태 확인을 위한 디버깅
+        #st.write(f"Is recording: {is_recording}, Byte array: {byte_array}")
+        
+        if byte_array:
 
-        if byte_array and isinstance(byte_array, list):
-            # byteArray가 list일 경우 이를 bytes로 변환
-            byte_array = bytes(byte_array)
+            if isinstance(byte_array, list):
+                # byteArray가 list일 경우 이를 bytes로 변환
+                byte_array = bytes(byte_array)
 
-        if byte_array and isinstance(byte_array, (bytes, bytearray)):
-            # 바이트 데이터를 BytesIO로 변환
-            audio_data = BytesIO(byte_array)
-            return audio_data
-        else:
-            st.error("Invalid byteArray or missing data.")
-    else:
-        st.error(f"Unexpected data format: {type(component_value)}")
+            if isinstance(byte_array, (bytes, bytearray)):
+                # 바이트 데이터를 BytesIO로 변환
+                audio_data = BytesIO(byte_array)
+                st.success("녹음이 완료되었습니다.")
+                return audio_data
+            else:
+                st.error("Invalid byteArray or missing data.")
+
+        if is_recording: # 녹음 중일 때
+            st.info("녹음이 끝나면 마이크를 한 번 더 눌러주세요.")
+        else: # 녹음 전일 때
+            st.error("마이크 버튼을 누르면 녹음이 시작됩니다.")
+            
     return None
